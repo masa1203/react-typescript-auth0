cba0a17ac83f49c13dbc84604d452d9270f55e05
masa1203 <msys.sawada@gmail.com>
2021-09-24 16:50:02 +0900

    ログインしたユーザー全員がトップページへ行けるようにした

--- react-auth0/src/App.tsx
+++ react-auth0/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import Config from "./config";
 
 function App() {
   const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
@@ -15,29 +14,23 @@ function App() {
   }
 
   if (isAuthenticated) {
-    if (user?.email !== Config.authenticatedUserEmail) {
-      return (
-        <div>
-          <p>あなたは利用できるユーザーではありません</p>
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            Log out
-          </button>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>React-TypeScript-Auth0</h1>
-          <p>UserName: {user?.name}</p>
-          <p>Email: {user?.email}</p>
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            Log out
-          </button>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <h1>React-TypeScript-Auth0</h1>
+        <p>UserName: {user?.name}</p>
+        <p>Email: {user?.email}</p>
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+          Log out
+        </button>
+      </div>
+    );
   } else {
-    return <button onClick={loginWithRedirect}>Login</button>;
+    return (
+      <div>
+        <h1>ログインが必要です</h1>
+        <button onClick={loginWithRedirect}>Login</button>
+      </div>
+    );
   }
 }
 
